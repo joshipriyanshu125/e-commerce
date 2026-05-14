7524575ac5b53f42ac17c99152fc0d8341f9af68
jojo <joshipriyanshu125@gmail.com>
2026-05-14 10:33:42 +0530

    Redis connection

---
+++ backend/src/utils/cache.js
@@ -0,0 +1,30 @@
+import redis from "../config/redis.js";
+
+export const setCache = async (key, data, expiry = 3600) => {
+    try {
+        await redis.set(key, JSON.stringify(data), "EX", expiry);
+    } catch (error) {
+        console.log("Cache Set Error:", error);
+    }
+};
+
+export const getCache = async (key) => {
+    try {
+        const data = await redis.get(key);
+
+        if (!data) return null;
+
+        return JSON.parse(data);
+    } catch (error) {
+        console.log("Cache Get Error:", error);
+        return null;
+    }
+};
+
+export const deleteCache = async (key) => {
+    try {
+        await redis.del(key);
+    } catch (error) {
+        console.log("Cache Delete Error:", error);
+    }
+};
